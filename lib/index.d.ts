461fb9f73c946cb1e4fe775607725f3aaebb343b
JudeBoy <judegiordano@gmail.com>
2021-08-13 13:18:23 -0400

    Export Generic Game Types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,10 @@
 import * as MLB from "./Types/Mlb";
 import * as NBA from "./Types/Nba";
+import * as Game from "./Types/Shared";
+export import ITeamInfo = Game.ITeamInfo;
+export import ISite = Game.ISite;
+export import IEvent = Game.IEvent;
+export import IOffical = Game.IOffical;
 export import IBatter = MLB.IBatter;
 export import IPitcher = MLB.IPitcher;
 export import IFielder = MLB.IFielder;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as MLB from "./Types/Mlb";
 import * as NBA from "./Types/Nba";
+import * as Game from "./Types/Shared";
 
+// game types
+export import ITeamInfo = Game.ITeamInfo;
+export import ISite = Game.ISite;
+export import IEvent = Game.IEvent;
+export import IOffical = Game.IOffical;
 // mlb types
 export import IBatter = MLB.IBatter;
 export import IPitcher = MLB.IPitcher;
